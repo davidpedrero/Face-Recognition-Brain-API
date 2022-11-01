ad4c940edaef18b9b9ef8c32252e1d7fa9612892
David Pedrero <pedrero888@gmail.com>
2022-11-01 15:39:18 -0400

    Adding profilrd to projet

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = knex({
 });
 
 db.select('*').from('users').then(data => {
-    console.log(data)
+    // console.log(data)
 });
 
 const app = express();
@@ -99,18 +99,17 @@ app.post('/register', (req, res) => {
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
 
-    let found = false;
-
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    })
-
-    if(!found) {
-        res.status(400).json("No such user");
-    }
+    db.select('*').from('users').where({id:id})
+        .then(user => {
+            if(user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('Not Found')
+            }
+        })
+        .catch(err => {
+            res.status(400).json('error getting user')
+        })
 })
 
 
