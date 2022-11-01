36167b70b0d7f8138e33c23056e119b0ae4de143
David Pedrero <pedrero888@gmail.com>
2022-11-01 15:01:50 -0400

    Added /register functionality

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const bodyParser = require('body-parser');
 const { json } = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+const { response } = require('express');
 
-const postgres = knex({
+const db = knex({
     client: 'pg',
     connection: {
         host : '127.0.0.1',
@@ -18,7 +19,9 @@ const postgres = knex({
     }
 });
 
-console.log(postgres.select('*').from('users'));
+db.select('*').from('users').then(data => {
+    console.log(data)
+});
 
 const app = express();
 
@@ -79,18 +82,17 @@ app.post('/register', (req, res) => {
         console.log(hash);
     });
 
-    database.users.push(
-        {
-            id: "125",
-            name: name,
-            email: email,
-            password: password,
-            entries: 0,
-            joined: new Date()
-        }
-    )
-
-    res.json(database.users[database.users.length - 1])
+    db('users')
+    .returning('*')
+    .insert({
+        email: email,
+        name: name,
+        joined: new Date()
+    })
+    .then(user => {
+        res.json(user[0])
+    })
+    .catch(err => res.status(400).json('Unable to register'))
 })
 
 
