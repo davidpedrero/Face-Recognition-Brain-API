46f0d82fb6527ed5dcb5fb7025d2ef941e98093e
David Pedrero <pedrero888@gmail.com>
2022-11-04 14:21:35 -0400

    fix5

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => { res.send('Success!') });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
-app.put('/image', (req, res) => { image.hangleImage(req, res, db)});
-app.post('/imageurl', (req, res) => { image.hangleAPICall(req, res) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
+app.post('/imageurl', (req, res) => { image.handleAPICall(req, res) })
 
 app.listen(process.env.PORT || 3000, () => { console.log(`App is running on port ${process.env.PORT}`) });
