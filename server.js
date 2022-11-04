7621b1962d1b68696f8b012d07156d20460d3300
David Pedrero <pedrero888@gmail.com>
2022-11-04 11:52:28 -0400

    adding port

--- server.js
+++ server.js
@@ -28,10 +28,11 @@ const db = knex({
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('Success') })
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
-app.put('/image', (req, res) => { image.hangleImage(req, res, db)})
+app.get('/', (req, res) => { res.send('Success') });
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
+app.put('/image', (req, res) => { image.hangleImage(req, res, db)});
+app.post('/imageurl', (req, res) => { image.hangleAPICall(req, res) })
 
-app.listen(3000, () => { console.log("App is running on port 3000") })
+app.listen(process.env.PORT || 3000, () => { console.log(`App is running on port ${process.env.PORT}`) });
